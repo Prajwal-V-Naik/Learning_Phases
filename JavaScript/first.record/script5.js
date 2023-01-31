aaa0fdd25924b92bcc5ec1fe77075b5dfc9dae70
Prajwal V Naik <prajwalv346@gmail.com>
2023-01-31 11:39:58 +0530

    Function learn

---
+++ JavaScript/first.record/script5.js
@@ -0,0 +1,16 @@
+// Function
+function helloJS(name){
+    console.log("Hello I'm learning functions()");
+    console.log(`Hello I'm learning ${name}`);
+
+}
+let n="callBackfunction";
+helloJS(n);
+
+function add(a,b,c){
+    return a + b + c;
+}
+console.log(add(1,2,3));
+
+var addition = add(4,5,6);
+console.log(addition);
