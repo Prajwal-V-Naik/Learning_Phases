890c8a4b1cc6454f1ff8d8fff8e6e331b96f34f9
Prajwal V Naik <prajwalv346@gmail.com>
2023-02-20 20:56:18 +0530

    React with chatGPT

--- ReactJS/secondReactJS/my-app/src/App.js
+++ ReactJS/secondReactJS/my-app/src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  function TodoItem({ title, description, onEdit, onDelete }) {
+    const [editMode, setEditMode] = useState(false);
+    const [newTitle, setNewTitle] = useState(title);
+    const [newDescription, setNewDescription] = useState(description);
+  
+    const handleTitleChange = (event) => {
+      setNewTitle(event.target.value);
+    };
+  
+    const handleDescriptionChange = (event) => {
+      setNewDescription(event.target.value);
+    };
+  
+    const handleEditClick = () => {
+      setEditMode(true);
+    };
+  
+    const handleCancelClick = () => {
+      setNewTitle(title);
+      setNewDescription(description);
+      setEditMode(false);
+    };
+  
+    const handleSaveClick = () => {
+      onEdit(newTitle, newDescription);
+      setEditMode(false);
+    };
+  
+    return (
+      <div>
+        {editMode ? (
+          <>
+            <input type="text" value={newTitle} onChange={handleTitleChange} />
+            <textarea
+              value={newDescription}
+              onChange={handleDescriptionChange}
+            />
+            <button onClick={handleSaveClick}>Save</button>
+            <button onClick={handleCancelClick}>Cancel</button>
+          </>
+        ) : (
+          <>
+            <h2>{title}</h2>
+            <p>{description}</p>
+            <button onClick={handleEditClick}>Edit</button>
+            <button onClick={onDelete}>Delete</button>
+          </>
+        )}
+      </div>
+    );
+  }
+  
+  function TodoList() {
+    const [todos, setTodos] = useState([]);
+  
+    const handleAddTodo = (title, description) => {
+      const newTodo = { title, description };
+      setTodos([...todos, newTodo]);
+    };
+  
+    const handleEditTodo = (index, title, description) => {
+      const newTodos = [...todos];
+      newTodos[index] = { title, description };
+      setTodos(newTodos);
+    };
+  
+    const handleDeleteTodo = (index) => {
+      const newTodos = [...todos];
+      newTodos.splice(index, 1);
+      setTodos(newTodos);
+    };
+  
+    return (
+      <div>
+        {todos.map((todo, index) => (
+          <div key={index}>
+            <TodoItem
+              title={todo.title}
+              description={todo.description}
+              onEdit={(newTitle, newDescription) =>
+                handleEditTodo(index, newTitle, newDescription)
+              }
+              onDelete={() => handleDeleteTodo(index)}
+            />
+          </div>
+        ))}
+        <button onClick={() => handleAddTodo('', '')}>Add Todo</button>
+      </div>
+    );
+  }
+  TodoList();
 }
 
 export default App;
