a96c113912f5a7bcea9159ebbd4acefd2a27ee5a
Prajwal V Naik <prajwalv346@gmail.com>
2023-02-23 15:58:51 +0530

    inheriance

--- JavaScript/first.record/script18.js
+++ JavaScript/first.record/script18.js
@@ -19,12 +19,30 @@ class SomeNew{
     getCourseList(){
         return this.#courseList;
     }
+    login(){
+        return "Hello logged in";
+    }
+}
+// Inheritance
+class SubAdmin extends SomeNew{
+    constructor(name,email){
+        super(name,email);
+    }
+    getAdminInfo(){
+        return "I am sub admin";
+    }
 }
 
 module.exports = SomeNew;
 
 const rock = new SomeNew("rock","rock@Gamil.com");
-console.log(rock.getInfo());;
+// console.log(rock.getInfo());;
 rock.enrollCourse("FSJS");
-console.log(rock.getCourseList());
-console.log(rock.courseList);
+// console.log(rock.getCourseList());
+// console.log(rock.courseList);
+
+
+const tom = new SubAdmin("tom","tom@tom");
+console.log(tom.getAdminInfo());
+console.log(tom.login());
+console.log(tom.getInfo());
