04c84c408b46d475d6500c1b81766c74bf1a1f9c
Prajwal V Naik <prajwalv346@gmail.com>
2023-02-28 18:16:29 +0530

    Live recorded class

---
+++ ReactJS/ReactLive/first-live-app/src/Components/App.js
@@ -0,0 +1,15 @@
+import Cards from './Cards';
+
+function App() {
+  return (
+    <>
+        <h1 className="text-red-900 font-bold bg-red-300 text-center py-1">Hello My name is Prajwal</h1>
+        <Cards name="ReactApp"/>
+        <Cards name="JS-Module"/>
+        <Cards name="ReduxJS"/>
+        <Cards name="JavaScript"/>
+    </>
+  );
+}
+
+export default App;
---
+++ ReactJS/ReactLive/first-live-app/src/Components/Cards.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Cards = ({name}) => (
+    <>
+        <p className='text-center my-3'>I'll develop something new using <span className='bg-yellow-200 rounded-lg p-1'>{name}</span></p>
+    </>
+)
+
+export default Cards
